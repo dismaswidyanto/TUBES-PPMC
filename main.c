dd6f4e0d47b65e3620a35d439c55e75a3dbdc46a
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2020-04-07 17:48:04 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
+#include<Windows.h>
 
 int main(){
+  int menu,n;
   
+  readfile();
+  printf("Menu\n");
+  printf("1. Tick\n");
+  printf("2. Animate\n");
+  printf("0. Quit\n");
+  scanf("%d",&menu);
+  
+  while(menu!=0){
+    if(menu==1){
+      tick();
+      display();
+    }
+    else if(menu==2){
+      printf("Masukkan Banyaknya Animasi: ");
+      scanf("%d",n);
+      
+      for(int i=0;i<n;i++){
+        tick();
+        display();
+        sleep(250);
+      }
+    }
+    printf("Menu\n");
+    printf("1. Tick\n");
+    printf("2. Animate\n");
+    printf("0. Quit\n");
+    scanf("%d",&menu);
+  }
 }
